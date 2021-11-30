f796202a2942575b0e6bc6b9209496d5fae06712
Baluev.mxntage <Baluev.mxntage@ART-BALUEV>
2021-11-30 04:37:51 +0300

    Add project files.

---
+++ 13/13.cpp
@@ -0,0 +1,21 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main() {
+    int n; 
+    cin >> n;
+    if (n % 2 == 0) ++n;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (j == n / 2 || i == n / 2) {
+                cout << "* ";
+            }
+            else {
+                cout << "  ";
+            }
+        }
+        cout << '\n';
+    }
+}
+
